7a1352c7dbaf20e971c645859bae62facd12df33
xdevx32 <angelkukushev333@gmail.com>
2017-03-21 21:41:45 +0200

    Implements some of the functions

--- src/ex1/Functions.scala
+++ src/ex1/Functions.scala
@@ -2,12 +2,46 @@ package ex1
 
 object Functions {
 
-  // Връща големината на масив (без да ползва data.length!!!)
-  def length(data: List[Int]) = ??? 
-
-  // Ако cond е true връща onTrue
-  def ifelse(cond: Boolean, onTrue: Int, onFalse: Int) = ???
-
+ def main(args: Array[String]) ={
+    println("Hello Scala!")
+    println(length(List(1,2,3,4,5)))
+    var x = 1
+    var xPlus = x + 1
+    println(ifelse(true,xPlus,x))
+    println(exists(List(1,2,3,4),greaterThan3))
+    println(forall(List(4,4,4,4),greaterThan3))
+    println(filter(List(4,3,3,3),List(),greaterThan3))
+  }
+  def length(data: List[Int], lengthCount: Int = 0): Int =
+    if(data.isEmpty) lengthCount
+    else length(data.tail, lengthCount+1)
+  
+  def ifelse(cond: Boolean, onTrue: => Int, onFalse: => Int)=
+    if(cond)
+      onTrue
+    else onFalse
+    
+  def exists(data: List[Int], f: (Int => Boolean)): Boolean =
+    if(data.isEmpty) false
+    else
+        if(f(data.head)) true
+        else exists(data.tail,f)
+  
+  def greaterThan3(x: Int): Boolean = x>3
+  
+  def forall(data: List[Int], f: (Int => Boolean)): Boolean =
+    if(length(data) == 1 && f(data.head)) true
+    else
+        if(f(data.head)) forall(data.tail,f)
+        else false
+    
+  def filter(data: List[Int],output: List[Int] ,f: (Int => Boolean)):List[Int] =
+    if(data.isEmpty) output
+    else
+      if(f(data.head)) output :+ data.head
+      else
+      filter(data.tail,output,f)
+    
   // Проверява дали скобите в даден масив от символи са балансирани.
   // Коректно: (a)asda(b)(v) | (((a))) | ()(()асдасд)
   // Грешно: )() | ((д) | ((das) (d)( 
@@ -20,15 +54,6 @@ object Functions {
   
     ???
   }
-
-  // Проверява дали съществува елемент отговарящ на f
-  def exists(data: List[Int], f: Any) = ???
-
-  // Връща масив съдържащ само елементите отговарящи на f
-  def filter(data: List[Int], f: Any) = ???
-
-  // Проверява дали всички елементи отговарят на f
-  def forall(data: List[Int], f: Any) = ???
 
   // Връща числото от триъгълника на Паскал отговарящо на съответния ред/колона
   def pascal(c: Int, r: Int): Int = ???
