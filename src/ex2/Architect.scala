441f6dc70e5c20787391271df07024f089d2fcd5
xdevx32 <angelkukushev333@gmail.com>
2017-04-04 13:05:02 +0300

    Initial commit

---
+++ src/ex2/Architect.scala
@@ -0,0 +1,45 @@
+package ex2
+
+sealed trait Shape
+case class Triangle(a: Int, b: Int, c: Int, h: Int) extends Shape // h represent the height against the longest side of the triangle
+case class Rectangle(a: Int, b: Int) extends Shape
+case class Trapezoid(a: Int, b: Int, h: Int) extends Shape
+case class Cube() extends Shape
+
+class Architect {
+
+  /*
+   *  Finds the max element from given list of integers.
+   *  The result is wrapped in an Option instance. The Option has two forms:
+   *   - None if no element satisfies the search criteria (for example, in case an empty list is provided)
+   *   - Some(x), where x if the searched element. In this case, it can be acquired with the method get. Example:
+   *     val o: Option[Int] = Some(6)
+   *     val n: Int = o.get
+   */
+  def max(xs: List[Int]): Option[Int] = ???
+
+  // Determines the type of given triangle: "rectangular", "equilateral", "isosceles", "random"
+  def triangleType(t: Triangle): String = ???
+
+  /*
+   * Calculates the area of the provided shape, by using these formulae:
+   *  - Rectangular triangle: a * b / 2, where a and b are cathetus
+   *  - Any triangle except rectangular: x * h / 2, where x is the largest side of the triangle and h is the opposite height
+   *  - Rectangle: a * b, where a and b are both sides
+   *  - Trapezoid: (a + b) * h / 2, where a and b are the parallel sides and h is the height between them
+   *  - Cube: always return -1
+   *  
+   *  Hint: for triangles use the max function
+   */
+  def area(s: Shape): Double = ???
+
+  /*
+   *  Returns the number of rectangular triangles in given list of shapes
+   *  
+   *  Hint: use the triangleType function
+   */
+  def findRectangulars(shapes: List[Shape]): Int = {
+    def iter(shapes: List[Shape], n: Int): Int = ???
+    iter(???, ???)
+  }
+}
