44347f22b5679fb1c8f38e5b7efb3d05b79ba587
xdevx32 <angelkukushev333@gmail.com>
2017-04-05 13:21:32 +0300

    Only findRectangulars not done

--- src/ex2/Architect.scala
+++ src/ex2/Architect.scala
@@ -1,5 +1,3 @@
-package ex2
-
 sealed trait Shape
 case class Triangle(a: Int, b: Int, c: Int, h: Int) extends Shape // h represent the height against the longest side of the triangle
 case class Rectangle(a: Int, b: Int) extends Shape
@@ -8,38 +6,62 @@ case class Cube() extends Shape
 
 class Architect {
 
-  /*
-   *  Finds the max element from given list of integers.
-   *  The result is wrapped in an Option instance. The Option has two forms:
-   *   - None if no element satisfies the search criteria (for example, in case an empty list is provided)
-   *   - Some(x), where x if the searched element. In this case, it can be acquired with the method get. Example:
-   *     val o: Option[Int] = Some(6)
-   *     val n: Int = o.get
-   */
-  def max(xs: List[Int]): Option[Int] = ???
+   def max(xs: List[Int]): Option[Int] = {
+      def iter(xs: List[Int],currentMax:Int): Option[Int] = {
+        if(xs.isEmpty) Some(currentMax) 
+        else {
+          iter(xs.tail,if (xs.head > currentMax) xs.head else currentMax);
+        }
+      }
+      if(xs.isEmpty) None
+      else iter(xs.tail, xs.head);
+    
+  }
 
-  // Determines the type of given triangle: "rectangular", "equilateral", "isosceles", "random"
-  def triangleType(t: Triangle): String = ???
+  def triangleType(t: Triangle): String =  {
+    if(!isTriangle(t)) return "not";
+    
+    if (t.a != t.b && t.b != t.c && t.a!=t.c){
+      val aSquare = t.a*t.a;
+      val bSquare = t.b*t.b;
+      val cSquare = t.c*t.c;
+  
+      if((aSquare + bSquare == cSquare)
+          || (aSquare + cSquare == bSquare) 
+          || (bSquare + cSquare == aSquare)) "rectangular" else "random";
+               
+    }else if(t.a == t.b && t.b == t.c) "equilateral";
+     else "isosceles";
+  }
 
-  /*
-   * Calculates the area of the provided shape, by using these formulae:
-   *  - Rectangular triangle: a * b / 2, where a and b are cathetus
-   *  - Any triangle except rectangular: x * h / 2, where x is the largest side of the triangle and h is the opposite height
-   *  - Rectangle: a * b, where a and b are both sides
-   *  - Trapezoid: (a + b) * h / 2, where a and b are the parallel sides and h is the height between them
-   *  - Cube: always return -1
-   *  
-   *  Hint: for triangles use the max function
-   */
-  def area(s: Shape): Double = ???
+  def area(s: Shape): Double = s match{
+    case t:Triangle => triangleType(t) match{
+      case "rectangular" => max(List(t.a,t.b,t.c)).get match{
+        case t.a => (t.b * t.c) / 2;
+        case t.b => (t.a * t.c) / 2;
+        case t.c => (t.a * t.b) / 2;
+      }
+      case "not" => -2;
+      case _ => (max(List(t.a,t.b,t.c)).get * t.h) .toDouble / 2;
+    }
+    case Rectangle(a,b) => a*b;
+    case Trapezoid(a,b,h) => (a + b).toDouble * h / 2;
+    case Cube() =>  -1;
+  }
+ 
+  def isTriangle(t:Triangle)={
+    if(t.a + t.b > t.c && t.b + t.c > t.a && t.a + t.c > t.b) true else false;
+  }
+}
 
-  /*
-   *  Returns the number of rectangular triangles in given list of shapes
-   *  
-   *  Hint: use the triangleType function
-   */
-  def findRectangulars(shapes: List[Shape]): Int = {
-    def iter(shapes: List[Shape], n: Int): Int = ???
-    iter(???, ???)
+object hwTwo {
+     def main(args: Array[String]) = {
+     var architect = new Architect
+     var triang = Triangle(3,4,5,4)
+     var rect = Rectangle(4,5)
+     println(architect.isTriangle(Triangle(10,500,20,4)))
+     println(architect.triangleType(Triangle(5,5,6,7)))
+     println(architect.triangleType(triang))
+     println(architect.area(rect))
   }
 }
