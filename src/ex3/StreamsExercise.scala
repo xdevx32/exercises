3cf048b169e691bdbd2ac32d35f06514f2c200db
xdevx32 <angelkukushev333@gmail.com>
2017-06-13 17:22:06 +0300

    Homework3 Fix

--- src/ex3/StreamsExercise.scala
+++ src/ex3/StreamsExercise.scala
@@ -1,7 +1,7 @@
 object StreamsExercise {
   
   def main(args: Array[String]): Unit = {
-    println("The fifth fibonacci number is " + fibFrom(1,1)(4))
+    //println("The fifth fibonacci number is " + fibFrom(1,1)(4))
    
 //    println("Task1 numbers are " + resultStreamTask1.toList)
     // the expected result is List(1, 153, 370, 371, 407)
@@ -97,20 +97,22 @@ object StreamsExercise {
     * @see do not use 'for' operators, implement it based on the streams of all natural numbers and
     *      find the desired number based on that stream.
     */
-  val capOfDigitsSizeTask1 = naturalNumbers(1).filter(x => x == sumOfDigitsPowThree(x))
-  // Testing
-  capOfDigitsSizeTask1.take(5).foreach(println)
+  val capOfDigitsSizeTask1 = from(1).take(10).toList.filter(x => math.pow(10, x) < x * math.pow(9,3) + 1).max + 1
+ 
+  
   /**
     * The stream of all numbers which may satisfy the TASK1 (defined above) conditions
     * @see Actually it is a stream computed from a range from 1 to the maximum number of
     *      the calculated capOfDigitsSizeTask1 digits.
     */
-  lazy val rangeStreamTask1 = capOfDigitsSizeTask1
-
+  lazy val rangeStreamTask1 = (1 to capOfDigitsSizeTask1 * math.pow(9, 3).toInt).toStream
+  
   /**
     * The stream of all numbers satisfying Task1 condition.
     */
-  lazy val resultStreamTask1 = capOfDigitsSizeTask1
-
+  lazy val resultStreamTask1 = rangeStreamTask1.filter(x => x == sumOfDigitsPowThree(x))
+  
+  
+    println("Task1 numbers are " + resultStreamTask1.toList)
   }
 }
